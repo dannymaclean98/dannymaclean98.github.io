e1d24d9041e716750c69893ae427a3ef828b0a60
Danny Maclean <dannymaclean98@gmail.com>
2018-11-11 11:37:17 -0600

    updates

--- app/components/coding/codingController.js
+++ app/components/coding/codingController.js
@@ -5,8 +5,9 @@
 app.controller('codingController', function($scope, $http, $log) {
 
     $scope.page = new Page('coding', {
-        url: 'assets/data/coding.json',
-        itemName: 'positiion'
+        itemName: 'positiion',
+        url: 'assets/data/coding.json'
+        
     });
 
 
